a0f1058181c68a733c4261c7a841fb0197ae86d6
Kachapin <kevinkachapin@gmail.com>
2022-12-06 01:53:38 +0300

    MEditing multiple files

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if sentence:
+        return (length, sentence[0])
+    else:
+        return (0, None)
+        
