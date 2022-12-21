c248a05c5db16aedc2ca835624928e78d8d2a952
Kachapin <kevinkachapin@gmail.com>
2022-12-21 03:42:16 +0300

    Trying an optimized code

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,27 +1,13 @@
-#!/usr/bin/python3
-"""Daclares a square"""
-
-
 class Square:
-    """Represents a square"""
-
     def __init__(self, size=0):
-        """Initialize a square
-
-            Args:
-                size: size of square
-
-        """
         self.size = size
 
     @property
     def size(self):
-        """Get the size of the square"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size of the square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -29,18 +15,15 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Area of the square
-        
-            Return:
-                Area of the square
-        """
-        return self._Square__size ** 2
+        return self.__size ** 2
 
     def my_print(self):
-        """prints a square with # characters"""
-        for i in range(self._Square__size):
-            for j in range(self._Square__size):
-                print("#", end="")
-            print()
         if self.__size == 0:
             print("")
+            return
+
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+            
