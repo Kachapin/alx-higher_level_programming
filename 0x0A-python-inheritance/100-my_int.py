811c8504fb6cbcbe2aa2dd345b38eee32e71e9b9
Kachapin <kevinkachapin@gmail.com>
2023-01-09 22:14:14 +0300

    More files

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Defines class MyInt"""
+
+
+class MyInt(int):
+    """Inverts == and !="""
+
+    def __eq__(self, other):
+        """Changes =="""
+        return int(self) != int(other)
+
+    def __ne__(self, other):
+        """Changes !="""
+        return int(self) == int(other)
+        
