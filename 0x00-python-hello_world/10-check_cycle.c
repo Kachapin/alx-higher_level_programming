c97c9c64cc35ac98cc15585592633810feba77bf
Kachapin <kevinkachapin@gmail.com>
2023-02-27 06:45:06 -0800

    Corrections

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,2 +1,23 @@
-#!/bin/bash
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp, *current;
+	int counter = 0, i;
+
+	current = list;
+	while(current)
+	{
+		temp = list;
+		for(i = 0; i < counter; i++)
+		{
+			if (current == temp)
+				return (1);
+			temp = temp->next;
+		}
+		counter++;
+		current = current->next;
+	}
+	return (0);
+}
 
